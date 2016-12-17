46dcc18ea89cc5560f19703647cd0aa895eabb1c
sxend <arimitsu.k+github@gmail.com>
2016-12-17 12:56:13 +0900

    add other datastore

--- scripts/cassandra.sh
+++ scripts/cassandra.sh
@@ -4,6 +4,13 @@ CLUSTER_NAME=$1
 
 NAME=internal-cassandra
 
+SEED_NAME=internal-cassandra-seed-0
+
+if [ "$(sudo docker ps -q --filter "NAME=${NAME}" | wc -l)" -eq "1" ]; then
+  sudo docker kill ${NAME}
+  SEED_NAME=internal-cassandra-seed-0
+fi
+
 if [ "$(sudo docker ps -q --filter "NAME=${NAME}" | wc -l)" -eq "1" ]; then
   sudo docker kill ${NAME}
 fi
@@ -16,7 +23,7 @@ BASE_DIR=$(eval echo ~$USER)/.${NAME} && \
 mkdir -p ${BASE_DIR}/var/lib/cassandra && \
 sudo docker run --name ${NAME} -it -d  \
   -e CASSANDRA_LISTEN_ADDRESS=auto \
-  -e CASSANDRA_SEEDS= \
+  -e CASSANDRA_SEEDS="$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' some-cassandra)" \
   -e CASSANDRA_CLUSTER_NAME=${CLUSTER_NAME} \
   -e CASSANDRA_NUM_TOKENS=512 \
   -v /my/own/datadir:/var/lib/cassandra \
--- scripts/rdb.sh
+++ scripts/rdb.sh
@@ -12,18 +12,17 @@ if [ "$(sudo docker ps -a -q --filter "NAME=${NAME}" | wc -l)" -eq "1" ]; then
   sudo docker rm ${NAME}
 fi
 
-
 BASE_DIR=$(eval echo ~$USER)/.${NAME} && \
 mkdir -p ${BASE_DIR}/var/lib/mysql && \
 mkdir -p ${BASE_DIR}/etc/mysql/conf.d && \
 aws s3 cp --recursive s3://internal-storage.arimit.su/${NAME}/etc/mysql/conf.d/ ${BASE_DIR}/etc/mysql/conf.d/ && \
-sudo docker pull mysql:8.0.0 && \
+sudo docker pull mysql:5.7.17 && \
 sudo docker run -it -d --name ${NAME} \
   -v ${BASE_DIR}/var/lib/mysql:/var/lib/mysql \
   -v ${BASE_DIR}/etc/mysql/conf.d:/etc/mysql/conf.d \
   -p 3306:3306 \
   -e MYSQL_ROOT_PASSWORD=$(aws s3 cp s3://internal-storage.arimit.su/${NAME}/root.pass - ) \
-  -e MYSQL_DATABASE=${} \
+  -e MYSQL_DATABASE=${DATABASE} \
   -e MYSQL_USER=$(aws s3 cp s3://internal-storage.arimit.su/${NAME}/user.name - ) \
   -e MYSQL_PASSWORD=$(aws s3 cp s3://internal-storage.arimit.su/${NAME}/user.pass - ) \
-  mysql:8.0.0
+  mysql:5.7.17
