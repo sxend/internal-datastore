464602bc22edff02c37128dfa9408f22ef298f40
sxend <arimitsu.k+github@gmail.com>
2016-12-17 18:09:41 +0900

    add sudo

--- scripts/cassandra.sh
+++ scripts/cassandra.sh
@@ -16,7 +16,7 @@ BASE_DIR=$(eval echo ~$USER)/.internal-datastore/${NAME} && \
 mkdir -p ${BASE_DIR}/var/lib/cassandra && \
 sudo docker run --name ${NAME} -it -d  \
   -e CASSANDRA_LISTEN_ADDRESS=auto \
-  -e CASSANDRA_SEEDS="$(docker inspect --format='{{ .NetworkSettings.IPAddress }}' ${NAME})" \
+  -e CASSANDRA_SEEDS="$(sudo docker inspect --format='{{ .NetworkSettings.IPAddress }}' ${NAME})" \
   -e CASSANDRA_CLUSTER_NAME=${CLUSTER_NAME} \
   -e CASSANDRA_NUM_TOKENS=512 \
   -v /my/own/datadir:/var/lib/cassandra \
